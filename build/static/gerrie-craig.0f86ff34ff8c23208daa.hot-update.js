fbf03ef1b4e120604610271436934696d1f652ea
Geraldine Craig <gerriemcraig@outlook.com>
2020-09-21 05:09:30 -0700

    Added styling

---
+++ build/static/gerrie-craig.0f86ff34ff8c23208daa.hot-update.js
@@ -0,0 +1,26 @@
+webpackHotUpdate("gerrie-craig",{
+
+/***/ "./packages/jsnation-theme/src/theme-files/index.js":
+/*!**********************************************************!*\
+  !*** ./packages/jsnation-theme/src/theme-files/index.js ***!
+  \**********************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\packages\\\\jsnation-theme\\\\src\\\\theme-files\\\\index.js: Expected corresponding JSX closing tag for <nav> (32:10)\\n\\n\\u001b[0m \\u001b[90m 30 | \\u001b[39m            \\u001b[33m<\\u001b[39m\\u001b[33mLink\\u001b[39m href\\u001b[33m=\\u001b[39m\\u001b[32m\\\"/page/2\\\"\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33mMore\\u001b[39m \\u001b[33mPosts\\u001b[39m\\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mLink\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 31 | \\u001b[39m            \\u001b[33m<\\u001b[39m\\u001b[33mLink\\u001b[39m href\\u001b[33m=\\u001b[39m\\u001b[32m\\\"/lorem-ipsum\\\"\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33mLorem\\u001b[39m \\u001b[33mIpsum\\u001b[39m\\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mLink\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 32 | \\u001b[39m          \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mHeaderContent\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    | \\u001b[39m          \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 33 | \\u001b[39m        \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mnav\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 34 | \\u001b[39m      \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mHeader\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 35 | \\u001b[39m      \\u001b[33m<\\u001b[39m\\u001b[33mmain\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n    at Object._raise (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:60:45)\\n    at Object.raiseWithData (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:55:17)\\n    at Object.raise (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:39:17)\\n    at Object.jsxParseElementAt (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\plugins\\\\jsx\\\\index.js:478:18)\\n    at Object.jsxParseElementAt (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\plugins\\\\jsx\\\\index.js:435:34)\\n    at Object.jsxParseElementAt (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\plugins\\\\jsx\\\\index.js:435:34)\\n    at Object.jsxParseElementAt (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\plugins\\\\jsx\\\\index.js:435:34)\\n    at Object.jsxParseElement (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\plugins\\\\jsx\\\\index.js:514:19)\\n    at Object.parseExprAtom (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\plugins\\\\jsx\\\\index.js:525:21)\\n    at Object.parseExprSubscripts (C:\\\\Users\\\\jugk\\\\Desktop\\\\gerrie-craig\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\expression.js:563:23)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3BhY2thZ2VzL2pzbmF0aW9uLXRoZW1lL3NyYy90aGVtZS1maWxlcy9pbmRleC5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./packages/jsnation-theme/src/theme-files/index.js\n");
+
+/***/ }),
+
+/***/ "./packages/jsnation-theme/src/theme-files/link.js":
+false,
+
+/***/ "./packages/jsnation-theme/src/theme-files/list.js":
+false,
+
+/***/ "./packages/jsnation-theme/src/theme-files/page.js":
+false,
+
+/***/ "./packages/jsnation-theme/src/theme-files/post.js":
+false
+
+})
--- packages/jsnation-theme/src/theme-files/index.js
+++ packages/jsnation-theme/src/theme-files/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "frontity";
+import { connect, Global, css, styled } from "frontity";
 import Link from "./link";
 import List from "./list";
 import Post from "./post";
@@ -9,21 +9,75 @@ const Root = ({ state }) => {
   const data = state.source.get(state.router.link);
   return (
     <>
-      <h1>Frontity Workshop</h1>
-      <p>Current URL: {state.router.link}</p>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/page/2">More Posts</Link>
-        <Link href="/lorem-ipsum">Lorem Ipsum</Link>
-      </nav>
-      <hr />
-      <main>
+      <Global
+        styles={css`
+          * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+          }
+          html {
+            font-family: sans-serif;
+          }
+        `}
+      />
+      <Header>
+        <HeaderContent>
+          <h1>Frontity Workshop</h1>
+          <p>Current URL: {state.router.link}</p>
+          <Menu>
+            <Link href="/">Home</Link>
+            <Link href="/page/2">More Posts</Link>
+            <Link href="/lorem-ipsum">Lorem Ipsum</Link>
+          </Menu>
+        </HeaderContent>
+      </Header>
+      <Main>
         {data.isArchive && <List />}
         {data.isPost && <Post />}
         {data.isPage && <Page />}
-      </main>
+      </Main>
     </>
   );
 };
 
 export default connect(Root);
+
+const Header = styled.header`
+  background-color: #eee;
+  border-width: 0 0 8px 0;
+  border-style: solid;
+  border-color: maroon;
+`;
+
+const HeaderContent = styled.div`
+  max-width: 800px;
+  padding: 2em 1em;
+  margin: auto;
+`;
+
+const Menu = styled.nav`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+  & > div {
+    margin-right: 1em;
+  }
+`;
+
+const Main = styled.main`
+  max-width: 800px;
+  padding: 1em;
+  margin: auto;
+
+  img {
+    max-width: 100%;
+  }
+  h2 {
+    margin: 0.5em 0;
+  }
+  p {
+    line-height: 1.25em;
+    margin-bottom: 0.75em;
+  }
+`;
